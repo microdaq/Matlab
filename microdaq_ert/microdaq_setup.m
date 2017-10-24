8ace4bc264d5128a1f1f11b0b51bb99af299f478
skibool <skiba.g@gmail.com>
2017-10-24 11:09:15 +0200

    Minor improvments in microdaq_setup

--- microdaq_ert/microdaq_setup.m
+++ microdaq_ert/microdaq_setup.m
@@ -20,6 +20,11 @@ addpref('microdaq','XDCRoot',XDCRoot);
 addpref('microdaq','BIOSRoot',BIOSRoot);
 % Ask for Target IP
 tip = inputdlg('Enter MicroDAQ IP address:','MicroDAQ IP Address',1,{'10.10.1.1'});
+ipAddr = regexp(tip{1}, '((0*(1\d\d|2[0-4]\d|25[0-4]|\d\d|\d)\.){3}0*(1\d\d|2[0-4]\d|25[0-4]|\d\d|\d))', 'match');
+if isempty(ipAddr)
+    error('Wrong IP address format!'); 
+end
+
 addpref('microdaq','TargetIP',tip{1});
 
 % Generate blocks
@@ -111,7 +116,6 @@ end
 
 cd(curpath);
 
-
 % Generate help
 %cd('../help/source');
 %genhelp;
@@ -125,10 +129,27 @@ cd([getpref('microdaq','TargetRoot'),'/../demos']);
 end
 
 function [CompilerRoot, XDCRoot, BIOSRoot] = ccs_setup_paths()
-% TODO: make it foolproof
-CompilerRoot = fix_slash(uigetdir(matlabroot,'Compiler root directory: (tools/compiler/c6000_7.X.X)'));
-XDCRoot = fix_slash(uigetdir(fileparts(CompilerRoot),'XDC Tools root directory: (xdctools_X_XX_XX_XX)'));
-BIOSRoot = fix_slash(uigetdir(fileparts(CompilerRoot),'SYS/BIOS root directory: (bios_6_XX_XX_XX)'));
+
+path = uigetdir(matlabroot,'Compiler root directory: (c6000_7.X.X)');
+if path == 0 
+    cd('..');
+    error('Abording microdaq_setup...'); 
+end
+CompilerRoot = fix_slash(path);
+
+path = uigetdir(fileparts(CompilerRoot),'XDC Tools root directory: (xdctools_X_XX_XX_XX)');
+if path == 0 
+    cd('..');
+    error('Abording microdaq_setup...'); 
+end
+XDCRoot = fix_slash(path);
+
+path = uigetdir(fileparts(CompilerRoot),'SYS/BIOS root directory: (bios_6_XX_XX_XX)');
+if path == 0 
+    cd('..');
+    error('Abording microdaq_setup...'); 
+end
+BIOSRoot = fix_slash(path);
 end
 
 function path = fix_slash(path0)
