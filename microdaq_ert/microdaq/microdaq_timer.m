a716854f477bf5db080787e864465665788dc7cc
witczenko <witczenko@gmail.com>
2017-11-29 19:44:12 +0100

    Add CPU detection to profiling feature

--- microdaq_ert/microdaq/microdaq_timer.m
+++ microdaq_ert/microdaq/microdaq_timer.m
@@ -13,14 +13,26 @@ hEnt.setTflCFunctionEntryParameters( ...
 
 hEnt.EntryInfo.CountDirection = 'RTW_TIMER_UP';
 hEnt.EntryInfo.TicksPerSecond = 300000000;
-% Conceptual Args
 
+% detect CPU 
+sysbiosPath =  fullfile(getpref('microdaq','TargetRoot'), 'sysbios.mk');
+fileID = fopen(sysbiosPath,'r');
+CPUSysbios = fgets(fileID);
+fclose(fileID);
+cpuVer = strfind(CPUSysbios,'cpu0');
+
+if (cpuVer ~= isempty(cpuVer))
+    hEnt.EntryInfo.TicksPerSecond = 300000000;
+else
+    hEnt.EntryInfo.TicksPerSecond = 456000000;
+end
+
+% Conceptual Args
 arg = hEnt.getTflArgFromString('y1','uint32');
 arg.IOType = 'RTW_IO_OUTPUT';
 hEnt.addConceptualArg(arg);
 
 % Implementation Args
-
 arg = hEnt.getTflArgFromString('y1','uint32');
 arg.IOType = 'RTW_IO_OUTPUT';
 hEnt.Implementation.setReturn(arg);
