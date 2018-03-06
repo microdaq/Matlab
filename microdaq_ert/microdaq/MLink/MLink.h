8907f35dcea71c709379f0be1a9cf1bee25d63fd
skibool <skiba.g@gmail.com>
2018-03-06 11:37:08 +0100

    Update MLink and DSP libs to 2.1.0v

--- microdaq_ert/microdaq/MLink/MLink.h
+++ microdaq_ert/microdaq/MLink/MLink.h
@@ -1,26 +1,19 @@
 /*
  * Copyright 2013-2017 Embedded Solutions
  *
- * File: MLink.h     $Revision: 2.0.0 $
+ * File: MLink.h     $Revision: 2.1.0 $
  *
  * Abstract:
  *	MLink function prototypes
  */
 
-#ifndef MLINK_H
-#define MLINK_H
+#ifndef _MLINK_H_
+#define _MLINK_H_
 
 #include <stdint.h>
 
-#define OUT
-#define IN
-#define IO
-
 #define EXTERNC
 #define MDAQ_API
-
-#define	AI_SINGLE	0 
-#define AI_DIFF		1
 
 /* Utility functions */ 
 EXTERNC MDAQ_API int mlink_connect( const char *addr, uint16_t port, int *link );
@@ -28,14 +21,20 @@ EXTERNC MDAQ_API int mlink_disconnect( int link_fd );
 EXTERNC MDAQ_API void mlink_disconnect_all( void );
 
 EXTERNC MDAQ_API char *mlink_error( int err );
-EXTERNC MDAQ_API char *mlink_version( int *link_fd );
+EXTERNC MDAQ_API int mlink_fw_version(int *link_fd, int *major, int *minor, int *fix, int *build);
+EXTERNC MDAQ_API int mlink_lib_version(int *link_fd, int *major, int *minor, int *fix, int *build);
+
 EXTERNC MDAQ_API int mlink_hwid( int *link_fd, int *hwid );
 EXTERNC MDAQ_API int mlink_fw_upload( int *link_fd, const char *fw_file);
 
 /* DSP handling functions */
+EXTERNC MDAQ_API int mlink_dsp_run(int *link_fd,  const char *dsp_binary_path, double period);
+EXTERNC MDAQ_API int mlink_dsp_signal_read(int signal_id, int signal_size, double *data, int data_size, int timeout);
+EXTERNC MDAQ_API int mlink_dsp_mem_write(int *link_fd, int start_idx, int len, float *data);
+EXTERNC MDAQ_API int mlink_dsp_stop(int *link_fd );
+
 EXTERNC MDAQ_API int mlink_dsp_load( int *link_fd, const char *dsp_binary_path, const char *args );
 EXTERNC MDAQ_API int mlink_dsp_start( int *link_fd );
-EXTERNC MDAQ_API int mlink_dsp_stop( int *link_fd );
 EXTERNC MDAQ_API int mlink_dsp_upload( int *link_fd );
 
 /* Digital IO functions */ 
@@ -66,12 +65,18 @@ EXTERNC MDAQ_API int mlink_pru_reg_write( int *link_fd, uint8_t pru_num, uint8_t
 EXTERNC MDAQ_API int mlink_pru_state_read( int *link_fd, uint8_t *pru0, uint8_t *pru1);
 
 /* AI functions */ 
-EXTERNC MDAQ_API int mlink_ai_read( int *link_fd, uint8_t *ch, uint8_t ch_count, double *range, uint8_t *mode, double *data );
-EXTERNC MDAQ_API int mlink_ai_scan_init(int *link_fd, uint8_t *ch, uint8_t ch_count, double *range, uint8_t *mode, float *rate, float duration);
-EXTERNC MDAQ_API int mlink_ai_scan(double *data, uint32_t scan_count, int32_t blocking);
-EXTERNC MDAQ_API int mlink_ai_scan_stop( void );
-EXTERNC MDAQ_API int mlink_ai_check_params(int *link_fd, uint8_t *ch, uint8_t ch_count, double *range, uint8_t *mode);
-EXTERNC MDAQ_API int mlink_ai_scan_get_ch_count(void);
+EXTERNC MDAQ_API int  mlink_ai_read( int *link_fd, uint8_t *ch, uint8_t ch_count, double *range, uint8_t *mode, double *data );
+EXTERNC MDAQ_API int  mlink_ai_scan_init(int *link_fd, uint8_t *ch, uint8_t ch_count, double *range, uint8_t *mode, float *rate, float duration);
+EXTERNC MDAQ_API int  mlink_ai_scan(double *data, uint32_t scan_count, int32_t blocking);
+EXTERNC MDAQ_API int  mlink_ai_scan_stop( void );
+EXTERNC MDAQ_API int  mlink_ai_check_params(int *link_fd, uint8_t *ch, uint8_t ch_count, double *range, uint8_t *mode);
+EXTERNC MDAQ_API int  mlink_ai_scan_get_ch_count(void);
+
+/* Recorder functions */ 
+
+EXTERNC MDAQ_API int mlink_recorder_start(int *link_fd, char *filename, char *comments, uint32_t has_comments, uint32_t is_csv, uint32_t is_seq, uint32_t time_format, uint32_t append, uint32_t time, uint32_t sec_between_scan, uint32_t ch_count, float rate, float duration); 
+EXTERNC MDAQ_API int mlink_recorder_stop(int *link_fd); 
+EXTERNC MDAQ_API int mlink_recorder_info(int *link_fd, uint8_t ch[], uint8_t ch_count, char *label, uint8_t len, uint32_t *done, uint32_t *status, uint32_t *remaining); 
 
 /* AO functions */ 
 EXTERNC MDAQ_API int mlink_ao_write( int *link_fd, uint8_t *ch, uint8_t ch_count, double *range, uint8_t mode, double *data );
@@ -81,4 +86,13 @@ EXTERNC MDAQ_API int mlink_ao_scan_stop(int *link_fd);
 EXTERNC MDAQ_API int mlink_ao_scan_data(int *link_fd, uint8_t *ch, int ch_count, float *data, int data_size, uint8_t opt);
 EXTERNC MDAQ_API int mlink_ao_check_params(int *link_fd, uint8_t *ch, uint8_t ch_count, double *range);
 
-#endif /* MLINK_H */ 
+/* PRU and MEM functions */
+EXTERNC MDAQ_API int mlink_get_obj_size( int *link_fd, char *var_name, uint32_t *size );
+EXTERNC MDAQ_API int mlink_get_obj( int *link_fd, char *obj_name, void *data, uint32_t size );
+EXTERNC MDAQ_API int mlink_set_obj( int *link_fd, char *obj_name, void *data, uint32_t size );
+EXTERNC MDAQ_API int mlink_mem_open( int *link_fd, uint32_t addr, uint32_t len );
+EXTERNC MDAQ_API int mlink_mem_close( int *link_fd, uint32_t addr, uint32_t len );
+EXTERNC MDAQ_API int mlink_mem_set( int *link_fd, uint32_t addr, int8_t *data, uint32_t len );
+EXTERNC MDAQ_API int mlink_mem_get( int *link_fd, uint32_t addr, int8_t *data, uint32_t len );
+
+#endif /* _MLINK_H_ */ 
